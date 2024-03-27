33a8405769d6bd88ac574b393a83f57c4d072a39
gmateus987 <gmateus987@gmail.com>
2024-03-27 14:52:49 -0300

    feat: fixing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    {
-        path: "home",
-        component: HomeComponent
-    }
+    {path: "", component: HomeComponent}
 ];
+
+@NgModule({
+    declarations: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
