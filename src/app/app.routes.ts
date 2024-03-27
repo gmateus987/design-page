93dd17b0ead3de96ec756807d1a7f597471dafd0
gmateus987 <gmateus987@gmail.com>
2024-03-27 15:55:26 -0300

    feat: added ssr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,9 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
+import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    {path: "", component: HomeComponent}
+    {
+        path: "",
+        component: HomeComponent
+    }
 ];
-
-@NgModule({
-    declarations: [],
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-})
-
-export class AppRoutingModule {}
